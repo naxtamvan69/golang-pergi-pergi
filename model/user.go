9e1fb8cccc570f89515334b3887fe775341d0053
Mochammad Naufal Rizki <mochammadnaufalrizki@gmail.com>
2022-12-09 22:45:20 +0700

    fix: add routing for update user, get all user, and delete user

--- model/user.go
+++ model/user.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type User struct {
-	ID             int       `gorm:"primaryKey" json:"id"`
-	Username       string    `gorm:"type:varchar(50);not null;unique" json:"username"`
-	Name           string    `gorm:"type:varchar(50);not null" json:"name"`
-	Age            int       `gorm:"type:integer;not null" json:"age"`
-	Sex            int       `gorm:"type:integer;not null" json:"sex"`
-	Password       string    `gorm:"type:varchar(255);not null" json:"password"`
+	ID             int       `gorm:"primaryKey" json:"id" binding:"required"`
+	Username       string    `gorm:"type:varchar(50);not null;unique" json:"username" binding:"required"`
+	Name           string    `gorm:"type:varchar(50);not null" json:"name" binding:"required"`
+	Age            int       `gorm:"type:integer;not null" json:"age" binding:"required"`
+	Sex            int       `gorm:"type:integer;not null" json:"sex" binding:"required"`
+	Password       string    `gorm:"type:varchar(255);not null" json:"password" binding:"required"`
 	RoleID         *int      `json:"role_id"`
 	TravelAgensiID *int      `json:"travel_agensi_id"`
 	CreatedAt      time.Time `json:"created_at"`
@@ -16,10 +16,12 @@ type User struct {
 }
 
 type UserRequest struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
-	Sex      int    `json:"sex"`
-	Password string `json:"password"`
+	ID             int    `json:"id" binding:"required"`
+	Username       string `json:"username" binding:"required"`
+	Name           string `json:"name" binding:"required"`
+	Age            int    `json:"age" binding:"required"`
+	Sex            int    `json:"sex" binding:"required"`
+	Password       string `json:"password" binding:"required"`
+	RoleID         *int   `json:"role_id"`
+	TravelAgensiID *int   `json:"travel_agensi_id"`
 }
